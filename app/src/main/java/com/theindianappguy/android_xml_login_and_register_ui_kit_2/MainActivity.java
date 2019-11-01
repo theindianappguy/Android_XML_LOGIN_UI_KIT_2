cb30834fa78809aa70bcc7f2a2f35276dcc405f0
theindianappguy <theindianappguy@gmail.com>
2019-11-01 20:01:14 +0530

    Initial commit

---
+++ app/src/main/java/com/theindianappguy/android_xml_login_and_register_ui_kit_2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.theindianappguy.android_xml_login_and_register_ui_kit_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
